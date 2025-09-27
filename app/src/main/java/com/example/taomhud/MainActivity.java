41d20aa7ebf5dd4d32d901ae2ddf2f4a3e9464c9
khanhngocnguyennuclst <nguyennukhanhngocclst@gmail.com>
2025-09-27 13:02:36 +0700

    lan 2

--- app/src/main/java/com/example/taomhud/MainActivity.java
+++ app/src/main/java/com/example/taomhud/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.taomhud;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_DangNhap, btn_DangKy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn_DangNhap=findViewById(R.id.btn_DangNhap);
+        btn_DangKy=findViewById(R.id.btn_DangKy);
+
+        btn_DangNhap.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
