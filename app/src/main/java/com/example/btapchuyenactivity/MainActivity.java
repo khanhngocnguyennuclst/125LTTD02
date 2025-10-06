27e01046b24b20cca94674a519594ded26d5ef38
khanhngocnguyennuclst <nguyennukhanhngocclst@gmail.com>
2025-10-07 00:05:30 +0700

    lan3

--- app/src/main/java/com/example/btapchuyenactivity/MainActivity.java
+++ app/src/main/java/com/example/btapchuyenactivity/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.btapchuyenactivity;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtText_Email, edtText_Password;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edtText_Email = findViewById(R.id.edtText_Email);
+        edtText_Password = findViewById(R.id.edtText_Password);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(v -> {
+            String email = edtText_Email.getText().toString().trim();
+            String pass = edtText_Password.getText().toString().trim();
+
+            if (email.equals("ngocdangiu") && pass.equals("ngocdangiu")) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                intent.putExtra("account_name", email);
+                startActivity(intent);
+            } else {
+                Toast.makeText(MainActivity.this, "Sai tài khoản hoặc mật khẩu!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
--- app/src/main/java/com/example/btapchuyenactivity/ProfileActivity.java
+++ app/src/main/java/com/example/btapchuyenactivity/ProfileActivity.java
@@ -1,4 +1,27 @@
 package com.example.btapchuyenactivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.Intent;
+public class ProfileActivity extends AppCompatActivity {
 
-public class ProfileActivity {
+    ImageView imgSetting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+
+        imgSetting = findViewById(R.id.imgSetting);
+
+
+        // Khi bấm hình bánh xe thì quay lại Login (MainActivity)
+        imgSetting.setOnClickListener(v -> {
+            Intent backIntent = new Intent(ProfileActivity.this, MainActivity.class);
+            backIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(backIntent);
+            finish();
+        });
+    }
 }
